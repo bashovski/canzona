6d2760a0776d1543b43c420537763dca1f92fd8f
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2020-02-20 22:00:39 +0100

    Add file upload to s3

---
+++ api/aws/s3/buckets.js
@@ -0,0 +1,4 @@
+module.exports = {
+    avatars: 'public/avatars',
+    tracks: 'public/tracks'
+};
--- api/aws/s3/s3.js
+++ api/aws/s3/s3.js
@@ -1,13 +1,36 @@
 const aws = require('aws-sdk');
+const fs = require('fs');
+const buckets = require('./buckets');
+const isImage = require('is-image');
+const files = require('../../services/crypt/files');
+
+const s3 = new aws.S3();
 
 module.exports = {
     instantiateS3(bucket) {
-        const s3 = new aws.S3();
         /*const response = s3.listObjectsV2({
             Bucket: bucket,
             Prefix: 'public/avatars/'
         }, (err, data) => {
             console.log(data);
         });*/
+    },
+
+    uploadFile(fileName, salt) {
+        const fileContent = fs.readFileSync(fileName);
+        if(!isImage(fileName)) throw 'invalid file passed';
+
+        files.generateHash(salt, (hash) => {
+            const delimited = fileName.split('.');
+
+            s3.upload({
+                Bucket: 'canzona',
+                Key: `${hash}.${delimited[delimited.length - 1]}`,
+                Body: fileContent
+            }, (err, data) => {
+                if(err) throw err;
+                console.log('successfully uploaded photo', data);
+            });
+        });
     }
 };
