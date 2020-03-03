f884c084e900170084a1ee967267528560e136ce
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2020-03-03 19:09:41 +0100

    Add EmailSent page

--- ui/src/pages/MailSent/index.js
+++ ui/src/pages/MailSent/index.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import Form from "../../components/Form";
+import Button from "../../components/Button";
 import './styles.scss';
 
 export default class MailSent extends Component {
     render() {
         return(
             <div className="cna-mail_sent">
-
+                <Form content={
+                    <div className="cna-mail_sent-items">
+                        <img src={require('../../assets/success.svg')} alt=""/>
+                        <p>Email successfully sent.</p>
+                        <Button link="/" value="Return to homepage"/>
+                    </div>
+                } />
             </div>
         );
     }
